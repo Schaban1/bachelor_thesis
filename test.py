bffbec71fdd20ee3d37175dfc4df1c858ee3a626
nhaH-luaP <paul.hahn@uni-kassel.de>
2024-11-26 17:08:48 +0100

    Some Debugging on the BO part

---
+++ test.py
@@ -0,0 +1,30 @@
+from prototype.user_profile_host import UserProfileHost
+import torch
+
+# TODO: This is for debugging, can be removed later.
+
+if __name__ == '__main__':
+    # Create a UserProfileHost()
+    user_profile_host = UserProfileHost(
+        original_prompt='A cute cat',
+        add_ons=None,
+        recommendation_type='bayes-opt'
+    )
+
+    # Define some specifications
+    num_recommendations = 5
+    beta = 20
+
+    # Play through an iteration loop
+    embeddings = user_profile_host.generate_recommendations(num_recommendations=num_recommendations, beta=beta)
+    preferences = torch.randint(0, 100, size=(num_recommendations,1)) / 10
+    for i in range(10):
+        # Reduce Beta
+        beta -= 1
+        # Update the user profile
+        user_profile_host.fit_user_profile(embeddings=embeddings, preferences=preferences)
+        # Generate new Recommendations
+        embeddings = user_profile_host.generate_recommendations(num_recommendations=num_recommendations, beta=beta)
+        # Evaluate new Recommendations
+        preferences = torch.randint(0, 100, size=(num_recommendations,1)) / 10
+    print("Test")
