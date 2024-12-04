2159fcbf9d5ffdf5a3451a3528dcc4622425c430
Niklas Deckers <249-deckersn@users.noreply.git.webis.de>
2024-12-04 09:38:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from omegaconf import DictConfig
 #def main(args):
 
 seed_everything(42)
-args = DictConfig({'path' : {'cache_dir' : './cache/'}, 'num_recommendations' : 1})
+args = DictConfig({'path' : {'cache_dir' : './cache/'}, 'num_recommendations' : 5})
 ui = WebUI(args=args)
 ui.run()
 
