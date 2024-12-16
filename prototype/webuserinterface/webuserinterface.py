49354e96fb244d56da24a25403befbba2ed6ffdd
Henry010303 <henry.plutz@gmail.com>
2024-12-16 22:26:38 +0100

    feat: Add number label to scores

--- prototype/webuserinterface/webuserinterface.py
+++ prototype/webuserinterface/webuserinterface.py
@@ -147,7 +147,7 @@ class WebUI:
                         self.images_display[i] = ngUI.interactive_image(self.images[i]).style(f'width: {self.image_display_size[0]}px; height: {self.image_display_size[1]}px; object-fit: scale-down')
                         with self.images_display[i]:
                             ngUI.button(icon='o_save', on_click=partial(self.on_save_button_click, self.images_display[i])).props('flat fab color=white').classes('absolute bottom-0 right-0 m-2')
-                        self.scores_toggles[i] = ngUI.toggle({0: '😢', 1: '🙁', 2: '😐', 3: '😄', 4: '😍'}, value=0).props('rounded')
+                        self.scores_toggles[i] = ngUI.toggle({0: '😢1', 1: '🙁2', 2: '😐3', 3: '😄4', 4: '😍5'}, value=0).props('rounded')
                         #self.scores_slider[i] = ngUI.slider(min=0, max=10, value=0, step=0.1)
                         #ngUI.label().bind_text_from(self.scores_slider[i], 'value')
             self.submit_button = ngUI.button('Submit scores', on_click=self.on_submit_scores_button_click)
