95f1fbc19ac336d9a01228bafc5f67dc9e4bebf0
nhaH-luaP <paul.hahn@uni-kassel.de>
2024-12-16 12:00:19 +0100

    changed standard args in main

--- main.py
+++ main.py
@@ -11,24 +11,24 @@ from omegaconf import DictConfig
 device = "cuda" if torch.cuda.is_available() else "cpu"
 args = DictConfig({
     'path' : {'cache_dir' : './cache/', 'images_save_dir': './saved_images/'}, 
-    'num_recommendations' : 3, 
+    'num_recommendations' : 5, 
     'port': 2048,
     'device': device,
     'random_seed' : 42, 
     'generator' : {
-        'num_inference_steps' : 3, 
+        'num_inference_steps' : 30, 
         'guidance_scale': 7.5, 
         'random_latents' : False,
-        'use_negative_prompt' : True
+        'use_negative_prompt' : False
     },
     'recommender' : {
         'extend_original_prompt' : True,
         'n_embedding_axis' : 10,
-        'embedding_bounds' : (-1., 1.),
+        'embedding_bounds' : (0., 1.),
         'use_embedding_center' : True,
         'n_latent_axis' : 3,
-        'latent_bounds' : (-1., 1.),
-        'use_latent_center' : False
+        'latent_bounds' : (0., 1.),
+        'use_latent_center' : True
     }
 })
 seed_everything(args.random_seed)
