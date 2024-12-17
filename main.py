6e28e784c8fd5bc4741afa1c519f576f164c3868
nhaH-luaP <paul.hahn@uni-kassel.de>
2024-12-17 15:41:40 +0100

    Solved the args problem

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from prototype.app import App
 from prototype.utils import seed_everything
 import hydra
+import os
 
-
-@hydra.main(version_base=None, config_path="/home/phahn/repositories/project-multimodal-machine-learning-lab-wise24/configs/", config_name="config")
+@hydra.main(version_base=None, config_path="configs/", config_name="config")
 def main(args):
     seed_everything(args.random_seed)
     app = App(args=args)
     app.start()
 
-
 if __name__ in {"__main__", "__mp_main__"}:
+    print(os.getcwd())
     main()
