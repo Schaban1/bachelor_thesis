f6f7b46b2913b832a2b19a903b8511d9157b3ace
Klara Gutekunst <klara.gutekunst.kg@gmail.com>
2024-12-18 16:01:49 +0100

    style: Uncomment tests

--- scripts/recommender_test.py
+++ scripts/recommender_test.py
@@ -8,12 +8,12 @@ if __name__ == '__main__':
     dummy_user_profile = torch.tensor([0.1, 0.2, 0.3])  # torch.tensor([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
     # test single point recommender
-    # single_recommender = SinglePointRecommender()
-    # single_point_recommendations = single_recommender.recommend_embeddings(user_profile=dummy_user_profile,
-    #                                                                        n_recommendations=200)
-    # if dummy_user_profile.shape[0] == 3:
-    #     display_generated_points(single_point_recommendations, user_profile=dummy_user_profile)
-    # print("single point", single_point_recommendations)
+    single_recommender = SinglePointRecommender()
+    single_point_recommendations = single_recommender.recommend_embeddings(user_profile=dummy_user_profile,
+                                                                           n_recommendations=200)
+    if dummy_user_profile.shape[0] == 3:
+        display_generated_points(single_point_recommendations, user_profile=dummy_user_profile)
+    print("single point", single_point_recommendations)
 
     # test single point + weighted axes recommender
     latent_bounds = (0., 1.)
