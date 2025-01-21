66a82e9c90e17c76e3cc06390a8af5c7bbcb9d0f
Henry010303 <henry.plutz@gmail.com>
2025-01-21 13:41:43 +0100

    feat: Add blind mode tooltip

--- prototype/webuserinterface/components/initial_iteration_ui.py
+++ prototype/webuserinterface/components/initial_iteration_ui.py
@@ -37,6 +37,7 @@ class InitialIterationUI(UIComponent):
                                     ngUI.switch("Blind Mode") \
                                         .classes('ml-2 mr-8') \
                                         .props('color=grey-8 checked-icon=visibility_off unchecked-icon=visibility') \
+                                        .tooltip('Randomly selects a recommendation type and keeps it hidden') \
                                         .bind_value(self.webUI, "blind_mode")
                     ngUI.separator().bind_visibility_from(self.webUI, 'blind_mode', value=False)
                     self.recommendation_field = ngUI.select({t: t.value for t in RecommendationType}) \
