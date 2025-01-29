47e327ce2ec8d59fdf6bcbbe04ce3f104b37feba
nhaH-luaP <paul.hahn@uni-kassel.de>
2025-01-29 18:47:41 +0100

    no one reads this messages anyways

--- prototype/webuserinterface/components/main_loop_ui.py
+++ prototype/webuserinterface/components/main_loop_ui.py
@@ -99,7 +99,7 @@ class MainLoopUI(UIComponent):
             self.webUI.num_images_to_generate = self.webUI.args.num_recommendations
 
             # Remove extra image spots in UI
-            for i in range(self.webUI.num_images_to_generate, len(self.image_display)):
+            for i in range(self.webUI.num_images_to_generate, len(self.webUI.image_display)):
                 self.webUI.images[i] = None
                 self.webUI.image_display[i] = None
             self.webUI.images = self.webUI.images[:self.webUI.num_images_to_generate]
