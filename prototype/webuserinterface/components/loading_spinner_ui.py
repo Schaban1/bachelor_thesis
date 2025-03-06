2fb85f31de0021e03ddfeb93d319cfc2247925be
Henry010303 <henry.plutz@gmail.com>
2025-03-06 18:45:46 +0100

    feat: Update progress calculation

--- prototype/webuserinterface/components/loading_spinner_ui.py
+++ prototype/webuserinterface/components/loading_spinner_ui.py
@@ -1,7 +1,10 @@
 from nicegui import ui as ngUI
+import math
 
 from prototype.webuserinterface.components.ui_component import UIComponent
 
+current_step = 0
+generator_num_batches = 1
 loading_progress = None
 
 
@@ -19,14 +22,17 @@ class LoadingSpinnerUI(UIComponent):
             ngUI.space().classes('m-4')
             global loading_progress
             loading_progress = ngUI.linear_progress(value=0, show_value=False, color='#323232')
+        global generator_num_batches
+        generator_num_batches = int(math.ceil(self.webUI.num_images_to_generate/self.webUI.args.generator.batch_size))
+
         self.webUI.generator.callback = update_progess
     
 def update_progess(pipe, step_index, timestep, callback_kwargs):
-    print("----------------------------------------------")
-    print(step_index)
-    print(timestep)
-    print(pipe.num_timesteps)
-    print("----------------------------------------------")
+    global current_step
+    global generator_num_batches
     global loading_progress
-    loading_progress.set_value(step_index/pipe.num_timesteps)
+    current_step += 1
+    loading_progress.set_value(current_step/(pipe.num_timesteps * generator_num_batches))
+    if current_step == (pipe.num_timesteps * generator_num_batches):
+        current_step = 0
     return callback_kwargs
