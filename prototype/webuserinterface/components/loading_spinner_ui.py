dc4a75d90b8cc56b6260a97eaff2e67e74340c1a
Henry010303 <henry.plutz@gmail.com>
2025-03-06 11:32:36 +0100

    feat: Test generator callback

--- prototype/webuserinterface/components/loading_spinner_ui.py
+++ prototype/webuserinterface/components/loading_spinner_ui.py
@@ -15,4 +15,9 @@ class LoadingSpinnerUI(UIComponent):
         with ngUI.column().classes('mx-auto items-center').bind_visibility_from(self.webUI, 'is_generating', value=True):
             ngUI.label('Generating images...').style('font-size: 200%;')
             ngUI.space().classes('m-4')
+            self.loading_progess = ngUI.linear_progress(value=0)
             ngUI.spinner(size='10em', color='#323232')
+        self.webUI.generator.callback = self.update_progess
+    
+    def update_progess(self, step, timestep, callback_kwargs):
+        self.loading_progess.set_value(step/self.webUI.args.generator.num_inference_steps)
