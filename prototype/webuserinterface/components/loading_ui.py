499fd79dd18b9c64d10a6af095e567aa32447341
Henry010303 <henry.plutz@gmail.com>
2025-04-04 23:13:27 +0200

    fix: Progress bar width

--- prototype/webuserinterface/components/loading_ui.py
+++ prototype/webuserinterface/components/loading_ui.py
@@ -20,8 +20,9 @@ class LoadingUI(UIComponent):
         with ngUI.column().classes('mx-auto items-center').bind_visibility_from(self.webUI, 'is_generating', value=True):
             ngUI.label('Generating images...').style('font-size: 200%;')
             ngUI.space().classes('m-4')
+        with ngUI.column().classes('w-200 items-center').bind_visibility_from(self.webUI, 'is_generating', value=True):
             global loading_progress
-            loading_progress = ngUI.linear_progress(value=0, show_value=False, color='#323232').classes('w-200').props('size=md;')
+            loading_progress = ngUI.linear_progress(value=0, show_value=False, color='#323232').props('size=md;')
         self.update_loading_batch_count()
         self.webUI.generator.callback = update_progess
     
