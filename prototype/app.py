f43d4309672571b3dd7f3c097aabb9b1f4e7cd21
Henry010303 <henry.plutz@gmail.com>
2025-04-23 15:11:04 +0200

    refactor: Put QLock implementation into utils

--- prototype/app.py
+++ prototype/app.py
@@ -1,47 +1,9 @@
 from nicegui import ui as ngUI
 import torch
 from diffusers import StableDiffusionPipeline, AutoencoderKL
-import threading
 
 from prototype.webuserinterface import WebUI
-
-import collections, time
-
-class QLock:
-    def __init__(self):
-        self.lock = threading.Lock()
-        self.waiters = collections.deque()
-        self.count = 0
-
-    def acquire(self):
-        self.lock.acquire()
-        if self.count:
-            new_lock = threading.Lock()
-            new_lock.acquire()
-            self.waiters.append(new_lock)
-            self.lock.release()
-            new_lock.acquire()
-            self.lock.acquire()
-        self.count += 1
-        self.lock.release()
-
-    def release(self):
-        with self.lock:
-            if not self.count:
-                raise ValueError("lock not acquired")
-            self.count -= 1
-            if self.waiters:
-                self.waiters.popleft().release()
-        time.sleep(0.01)
-
-    def locked(self):
-        return self.count > 0
-    
-    def __enter__(self):
-        self.acquire()
-    
-    def __exit__(self, type, val, traceback):
-        self.release()
+from prototype.utils import QLock
 
 global_args = None
 pipe = None
